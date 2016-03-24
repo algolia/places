d250c83a5cec896ee310d7d800cd52d7f6b32663
vvo <vincent.voyer@algolia.com>
2016-03-24 16:16:04 +0100

    feat(icons): add city and address icons, inlined in JS

---
+++ src/icons.js
@@ -0,0 +1,9 @@
+// svg will be inlined by the raw-loader (see /webpack.config.babel.js)
+
+import address from './icons/address.svg';
+import city from './icons/city.svg';
+
+export default {
+  address,
+  city
+};
