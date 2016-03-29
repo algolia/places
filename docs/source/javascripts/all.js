f33a5bd2f4c011579e6a1911fa38e5ef7c434394
Lucas Bonomi <lucas.bonomi@gmail.com>
2016-03-29 13:53:54 +0200

    First commit on new documentation branch

--- docs/source/javascripts/all.js
+++ docs/source/javascripts/all.js
@@ -1,24 +1,19 @@
-/* global places */
+places({container: document.querySelector('#landing-demo')});
 
-places({
-  container: document.querySelector('#landing-demo')
-});
+document.addEventListener('scroll', function(event){
+	const hero = document.querySelector('.hero-section');
+	const navigation = document.querySelector('.navigation') ;
 
-// automatically add necessary css classes for responsive layour
-document.addEventListener('scroll', scrollEvent => {
-  const hero = document.querySelector('.hero-section');
-  const navigation = document.querySelector('.navigation');
+	const value = event.target.scrollingElement.scrollTop;
+	const height = hero.offsetHeight;
+	const navHeight = navigation.offsetHeight;
 
-  const value = scrollEvent.target.scrollingElement.scrollTop;
-  const height = hero.offsetHeight;
-  const navHeight = navigation.offsetHeight;
-
-  if (value > height) {
-    navigation.classList.add('darken');
-  } else if (value > (height - navHeight)) {
-    navigation.classList.add('init');
-    navigation.classList.remove('darken');
-  } else {
-    navigation.classList.remove('darken', 'init');
-  }
-});
+	if(value > height) {
+		navigation.classList.add('darken');
+	} else if(value > (height - navHeight)) {
+		navigation.classList.add('init');
+		navigation.classList.remove('darken');
+	} else {
+		navigation.classList.remove('darken','init');
+	}
+})
