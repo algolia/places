efe858dc3a8111acb3f5efa36587cff6e16b9aff
Lucas Bonomi <lucas.bonomi@gmail.com>
2016-03-29 13:53:54 +0200

    docs(documentation): first iteration on /documentation, including mobile

--- docs/source/javascripts/all.js
+++ docs/source/javascripts/all.js
@@ -4,14 +4,18 @@ places({
   container: document.querySelector('#landing-demo')
 });
 
-// automatically darken the top menu when going down
-document.addEventListener('scroll', scrollEvent => {
-  const hero = document.querySelector('.hero-section');
-  const navigation = document.querySelector('.navigation');
+// The following code is used to change the color of the navigation
+// depending the level of page scroll.
+const hero = document.querySelector('.hero-section');
+const navigation = document.querySelector('.navigation');
+const height = hero.offsetHeight;
+const navHeight = navigation.offsetHeight;
+const sidebar = document.getElementById('sidebar');
 
-  const value = scrollEvent.target.scrollingElement.scrollTop;
-  const height = hero.offsetHeight;
-  const navHeight = navigation.offsetHeight;
+
+// automatically darken the top menu when going down
+document.addEventListener('scroll', () => {
+  const value = event.target.scrollingElement.scrollTop;
 
   if (value > height) {
     navigation.classList.add('darken');
@@ -21,4 +25,51 @@ document.addEventListener('scroll', scrollEvent => {
   } else {
     navigation.classList.remove('darken', 'init');
   }
+
+  const doc = document.querySelector('.documentation-section');
+  if (doc) {
+    let paddingDoc = window.getComputedStyle(doc, null).getPropertyValue('padding-top').split('px')[0];
+    paddingDoc = parseInt(paddingDoc, 10);
+    // Fix the sidebar navigation
+    if (value > ((height - navHeight) + paddingDoc) && sidebar) {
+      sidebar.classList.add('fixed');
+    } else {
+      sidebar.classList.remove('fixed');
+    }
+  }
+});
+// Responsive navigation
+const theSelect = document.createElement('select');
+
+function selectizer() {
+   // Let's make the select
+  var isSelect = document.getElementById('selectNav');
+  theSelect.id = 'selectNav';
+  theSelect.classList.add('display-on-small');
+
+  if (!isSelect) {
+    document.querySelector('.navigation').appendChild(theSelect);
+  }
+
+  const links = document.querySelectorAll('.menu li a');
+  var home = document.createElement('option');
+  home.text = 'Homepage';
+  home.value = 'index.html';
+  theSelect.appendChild(home);
+  for (let i = 0; i < links.length; i++) {
+    let option = document.createElement('option');
+    option.text = links[i].textContent;
+    option.value = links[i].href;
+    theSelect.appendChild(option);
+  }
+}
+
+window.onload = function() {
+  selectizer();
+};
+
+
+theSelect.addEventListener('change', () => {
+  var value = this.options[this.selectedIndex].value;
+  window.location = value;
 });
