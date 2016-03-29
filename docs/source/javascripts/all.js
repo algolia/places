51c13ef81fdc8584bf0f8a912abe848e91bc0426
Lucas Bonomi <lucas.bonomi@gmail.com>
2016-03-29 13:53:54 +0200

    First commit on new documentation branch

--- docs/source/javascripts/all.js
+++ docs/source/javascripts/all.js
@@ -4,21 +4,24 @@ places({
   container: document.querySelector('#landing-demo')
 });
 
+document.addEventListener('scroll', function(event){
+	const hero = document.querySelector('.hero-section');
+	const navigation = document.querySelector('.navigation') ;
+
 // automatically darken the top menu when going down
 document.addEventListener('scroll', scrollEvent => {
   const hero = document.querySelector('.hero-section');
   const navigation = document.querySelector('.navigation');
+	const value = event.target.scrollingElement.scrollTop;
+	const height = hero.offsetHeight;
+	const navHeight = navigation.offsetHeight;
 
-  const value = scrollEvent.target.scrollingElement.scrollTop;
-  const height = hero.offsetHeight;
-  const navHeight = navigation.offsetHeight;
-
-  if (value > height) {
-    navigation.classList.add('darken');
-  } else if (value > (height - navHeight)) {
-    navigation.classList.add('init');
-    navigation.classList.remove('darken');
-  } else {
-    navigation.classList.remove('darken', 'init');
-  }
-});
+	if(value > height) {
+		navigation.classList.add('darken');
+	} else if(value > (height - navHeight)) {
+		navigation.classList.add('init');
+		navigation.classList.remove('darken');
+	} else {
+		navigation.classList.remove('darken','init');
+	}
+})
