0fc4e5aaf79e2ce184856714ce7584483b0ab68f
Lucas Bonomi <lucas.bonomi@gmail.com>
2016-03-29 17:14:44 +0200

    Documentation done

--- docs/source/javascripts/all.js
+++ docs/source/javascripts/all.js
@@ -4,17 +4,20 @@ places({
   container: document.querySelector('#landing-demo')
 });
 
-document.addEventListener('scroll', function(event){
-	const hero = document.querySelector('.hero-section');
-	const navigation = document.querySelector('.navigation') ;
+// The following code is used to change the color of the navigation
+// depending the level of page scroll.
+const hero = document.querySelector('.hero-section');
+const navigation = document.querySelector('.navigation') ;
+const height = hero.offsetHeight;
+const navHeight = navigation.offsetHeight;
+const sidebar = document.getElementById('sidebar');
+
 
 // automatically darken the top menu when going down
 document.addEventListener('scroll', scrollEvent => {
   const hero = document.querySelector('.hero-section');
   const navigation = document.querySelector('.navigation');
 	const value = event.target.scrollingElement.scrollTop;
-	const height = hero.offsetHeight;
-	const navHeight = navigation.offsetHeight;
 
 	if(value > height) {
 		navigation.classList.add('darken');
@@ -24,4 +27,24 @@ document.addEventListener('scroll', scrollEvent => {
 	} else {
 		navigation.classList.remove('darken','init');
 	}
+
+	const doc = document.querySelector('.documentation-section');
+	if(doc){
+		const paddingDoc = window.getComputedStyle(doc, null).getPropertyValue('padding-top').split('px')[0];
+		// Fix the sidebar navigation
+		if(value > ((height - navHeight) + parseInt(paddingDoc)) && sidebar) {
+			sidebar.classList.add('fixed')
+		} else {
+			sidebar.classList.remove('fixed')
+		}
+	}
 })
+
+// document.addEventListener('scroll', function(event){
+// 	const valuex = event.target.scrollingElement.scrollTop;
+// 	if(valuex > ((height - navHeight) + parseInt(paddingDoc)) && sidebar) {
+// 		sidebar.classList.add('fixed')
+// 	} else {
+// 		sidebar.classList.remove('fixed')
+// 	}
+// })
