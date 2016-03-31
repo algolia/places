1c33593d9ce05cf9fa7d89f806686a410465f28e
Lucas Bonomi <lucas.bonomi@gmail.com>
2016-03-31 14:20:41 +0200

    Fix JS with firefox, now using requestAnimationFrames for the animations

--- docs/source/javascripts/all.js
+++ docs/source/javascripts/all.js
@@ -14,8 +14,27 @@ const sidebar = document.getElementById('sidebar');
 
 
 // automatically darken the top menu when going down
-document.addEventListener('scroll', scrollEvent => {
-  const value = scrollEvent.target.scrollingElement.scrollTop;
+// Using requestAnimationFrame
+const doc = document.querySelector('.documentation-section');
+
+
+let [latestKnownScrollY, ticking] = [0, false];
+
+function onScroll() {
+  latestKnownScrollY = window.scrollY;
+  requestTick();
+}
+
+function requestTick() {
+  if (!ticking) {
+    requestAnimationFrame(updateScroll);
+  }
+  ticking = true;
+}
+function updateScroll() {
+  ticking = false;
+  var currentScrollY = latestKnownScrollY;
+  const value = currentScrollY;
 
   if (value > height) {
     navigation.classList.add('darken');
@@ -26,7 +45,6 @@ document.addEventListener('scroll', scrollEvent => {
     navigation.classList.remove('darken', 'init');
   }
 
-  const doc = document.querySelector('.documentation-section');
   if (doc) {
     let paddingDoc = window.getComputedStyle(doc, null).getPropertyValue('padding-top').split('px')[0];
     paddingDoc = parseInt(paddingDoc, 10);
@@ -37,7 +55,10 @@ document.addEventListener('scroll', scrollEvent => {
       sidebar.classList.remove('fixed');
     }
   }
-});
+}
+window.addEventListener('scroll', onScroll, false);
+
+
 // Responsive navigation
 const theSelect = document.createElement('select');
 
