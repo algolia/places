3d3233f6618109e81555dde8bac219ca6b360297
Vincent Voyer <vincent.voyer@algolia.com>
2016-04-05 18:31:41 +0200

    feat(events): add `suggestions` and `cursorchanged` events

--- docs/source/javascripts/index.js
+++ docs/source/javascripts/index.js
@@ -3,6 +3,22 @@
 
 import './responsive-navigation.js';
 
-places({
+const placesAutocomplete = places({
   container: document.querySelector('#landing-demo')
-}).on('change', suggestion => console.log(suggestion));
+});
+
+const events = ['change', 'suggestions', 'cursorchanged'];
+events.forEach(eventName =>
+  placesAutocomplete.on(eventName, eventData => {
+    if (!Array.isArray(eventData)) {
+      eventData = [eventData];
+    }
+
+    console.log(`Algolia Places: received event **${eventName}**`);
+    if (typeof console.table === 'function') {
+      console.table(eventData);
+    } else {
+      console.log('event data:', eventData);
+    }
+  })
+);
