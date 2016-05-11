2cb1e544bdf85974b6177ff8f4e7faab244bac01
vvo <vincent.voyer@algolia.com>
2016-05-11 15:04:24 +0200

    fix(utm): compute same utm as docsearch

--- src/defaultTemplates.js
+++ src/defaultTemplates.js
@@ -5,7 +5,7 @@ import osmLogo from './icons/osm.svg';
 
 export default {
   footer: `<div class="ap-footer">
-  Built by <a href="https://www.algolia.com/?UTMFIXME" title="Search by Algolia" class="ap-footer-algolia">${algoliaLogo.trim()}</a>
+  Built by <a href="https://www.algolia.com/?utm_source=places&utm_medium=link&utm_term=footer&utm_campaign=${location.hostname}" title="Search by Algolia" class="ap-footer-algolia">${algoliaLogo.trim()}</a>
   using <a href="https://community.algolia.com/places/documentation.html#license" class="ap-footer-osm" title="Algolia Places data © OpenStreetMap contributors">${osmLogo.trim()} <span>data</span></a>
   </div>`,
   inputValue,
