8c1c6c2787e431d0a9eb9150c852c123d39c3ed0
vvo <vincent.voyer@algolia.com>
2016-05-12 10:37:07 +0200

    docs(docsearch): activate everywhere

--- docs/source/javascripts/examples.js
+++ docs/source/javascripts/examples.js
@@ -2,6 +2,7 @@ import anchorableElements from './anchorableElements.js';
 import responsiveNavigation from './responsiveNavigation.js';
 import sidebar from './sidebar.js';
 import activateClipboard from './activateClipboard.js';
+import './docsearch.js';
 
 responsiveNavigation();
 sidebar({
