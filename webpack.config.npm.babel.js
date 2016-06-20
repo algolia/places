275f493438420e644518c9dda6745886efe73bd2
Vincent Voyer <wouaren@gmail.com>
2016-06-20 13:55:04 +0200

    feat(npm): provide a nice and clean npm build (#237)
    The default npm export will now be an ES5 source tree with webpack
    loaders resolved

---
+++ webpack.config.npm.babel.js
@@ -0,0 +1,12 @@
+import baseConfig from './webpack.config.babel.js';
+
+// https://github.com/istarkov/babel-plugin-webpack-loaders#how-it-works
+
+export default {
+  ...baseConfig,
+  entry: undefined,
+  devtool: undefined,
+  output: {
+    libraryTarget: 'commonjs2'
+  }
+};
