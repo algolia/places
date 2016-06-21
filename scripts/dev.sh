80823f0e2ede2e22e63fa1c7661c0d12db51abae
vvo <vincent.voyer@algolia.com>
2016-06-21 16:15:18 +0200

    chore(dev): npm install before using json

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -4,9 +4,10 @@ set -e # exit when error
 
 printf "\nLaunching dev environment\n"
 
+npm install
+
 VERSION=$(json version < package.json)"-DEV"
 
-npm install &&
 cd docs &&
 bundle install &&
 NODE_ENV=development VERSION=$VERSION bundle exec middleman
