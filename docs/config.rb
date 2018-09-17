cf2f7c3c0cbc9438d6e6930466eb8cf1e70386d6
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-09-17 11:22:25 +0200

    docs: add Google API Key

--- docs/config.rb
+++ docs/config.rb
@@ -32,7 +32,7 @@ configure :development do
   config[:places_autocomplete_dataset_lib_url] = 'placesAutocompleteDataset'
   config[:places_instantsearch_widget_lib_url] = 'placesInstantsearchWidget'
   config[:instantsearch_lib_url] = 'https://cdn.jsdelivr.net/instantsearch.js/2.10.1/instantsearch.min.js'
-  config[:google_maps_lib_url] = 'https://maps.googleapis.com/maps/api/js'
+  config[:google_maps_lib_url] = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBawL8VbstJDdU5397SUX7pEt9DslAwWgQ'
   activate :external_pipeline,
     name: 'places',
     command: 'npm run js:watch -- --output-path docs/.webpack/js',
