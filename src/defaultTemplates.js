009d26c2a15efd559b144b86235896aea75579eb
Eric Quanz <eric.quanz@gmail.com>
2019-01-18 14:31:20 +0100

    fix(ie11-logo): fix algolia logo rendering on IE 11 (#575)
    * fix: algolia logo rendering on IE 11
    
    * fix(widget-footer): Use latest version of logo, fix vertical-alignment

--- src/defaultTemplates.js
+++ src/defaultTemplates.js
@@ -5,7 +5,7 @@ import osmLogo from './icons/osm.svg';
 
 export default {
   footer: `<div class="ap-footer">
-  Built by <a href="https://www.algolia.com/places" title="Search by Algolia" class="ap-footer-algolia">${algoliaLogo.trim()}</a>
+  <a href="https://www.algolia.com/places" title="Search by Algolia" class="ap-footer-algolia">${algoliaLogo.trim()}</a>
   using <a href="https://community.algolia.com/places/documentation.html#license" class="ap-footer-osm" title="Algolia Places data © OpenStreetMap contributors">${osmLogo.trim()} <span>data</span></a>
   </div>`,
   value,
