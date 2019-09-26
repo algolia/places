f0c7ac53cfffb68488f4e1077febc072098d5a67
Jonathan Montane <jonathan.montane@algolia.com>
2019-09-26 13:57:17 +0200

    fix(autocomplete-css): include babel-css into dist folder

--- scripts/build.sh
+++ scripts/build.sh
@@ -45,6 +45,7 @@ done
 # The goal is for the user to be able to require/import places without
 # having to import a complete build (dist/cdn), to avoid duplication of dependencies
 BABEL_DISABLE_CACHE=1 BABEL_ENV=npm babel index.js -o "$dist_dir/index.js"
+BABEL_DISABLE_CACHE=1 BABEL_ENV=npm babel babel-css.js -o "$dist_dir/babel-css.js"
 BABEL_DISABLE_CACHE=1 BABEL_ENV=npm babel autocompleteDataset.js -o "$dist_dir/autocompleteDataset.js"
 BABEL_DISABLE_CACHE=1 BABEL_ENV=npm babel instantsearchWidget.js -o "$dist_dir/instantsearchWidget.js"
 BABEL_DISABLE_CACHE=1 BABEL_ENV=npm babel src/ --out-dir "$dist_dir/src/" --ignore "src/**/*.test.js","src/**/__mocks__/*","src/**/__snapshots__/*"
